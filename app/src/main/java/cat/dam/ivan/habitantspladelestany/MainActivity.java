9fb18b39182768cdd20c2203ada7df9496cced1b
Ivan <79653212+Ivan21907@users.noreply.github.com>
2022-02-09 15:28:49 +0100

    Programa millorat

--- app/src/main/java/cat/dam/ivan/habitantspladelestany/MainActivity.java
+++ app/src/main/java/cat/dam/ivan/habitantspladelestany/MainActivity.java
@@ -36,55 +36,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view)
             {
                 //bucle que recorre totes les posicions de les dos arrays
-                for(int i = 0; i <= municipis.length && i<= num_Habitants.length; i++)
+                for(int i = 0; i < municipis.length; i++)
                 {
-                    if(municipis[0].equals("Banyoles"))
+                    if(municipis[i].equals(atv_Municipi.getText().toString()))
                     {
-                        num_hab.setText("17451");
-                    }
-                    else if(municipis[1].equals("Camós"))
-                    {
-                        num_hab.setText("698");
-                    }
-                    else if(municipis[2].equals("Cornellà del Terri"))
-                    {
-                        num_hab.setText("2106");
-                    }
-                    else if(municipis[3].equals("Crespià"))
-                    {
-                        num_hab.setText("247");
-                    }
-                    else if(municipis[4].equals("Esponellà"))
-                    {
-                        num_hab.setText("441");
-                    }
-                    else if(municipis[5].equals("Fontcoberta"))
-                    {
-                        num_hab.setText("1212");
-                    }
-                    else if(municipis[6].equals("Palol de Revardit"))
-                    {
-                        num_hab.setText("459");
-                    }
-                    else if(municipis[7].equals("Porqueres"))
-                    {
-                        num_hab.setText("4208");
-                    }
-                    else if(municipis[8].equals("Sant Miquel de Campmajor"))
-                    {
-                        num_hab.setText("218");
-                    }
-                    else if(municipis[9].equals("Serinyà"))
-                    {
-                        num_hab.setText("1084");
-                    }
-                    else if(municipis[10].equals("Vilademuls"))
-                    {
-                        num_hab.setText("769");
+                        num_hab.setText(num_Habitants[i]);
                     }
                 }
             }
         });
-
     }
 }
